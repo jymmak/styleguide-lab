a4eabc53da19017053774ddedcb03b54062f51ed
Jymma Karina Mogollon Cardoza <henryfrancisco_fernando@hotmail.com>
2018-03-27 23:20:06 -0500

    avance componentes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
+
+
 
 // Components
 import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-      </div>
-    );
-  }
-}
+
+
+const App = () => (
+
+  <div className="App">
+    <Header />
+    <Main />
+    <Footer />
+ 
+  </div>
+
+);
+
 
 export default App;
