e65e0495891b1ef806add71401ede8f0f0a9d809
Jymma Karina Mogollon Cardoza <henryfrancisco_fernando@hotmail.com>
2018-03-28 00:17:17 -0500

    aside router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
 import React from 'react';
-
+import { Redirect, Link, Route, Switch } from "react-router-dom";
+import './App.css';
 
 
 // Components
 import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
-
+import About from "./components/About";
+import Botons from "./components/Botons";
+import Fonts from "./components/Fonts";
+import Cards from "./components/Cards";
 
 
 const App = () => (
 
   <div className="App">
     <Header />
-    <Main />
-    <Footer />
- 
+    <div>
+      <nav class="grupo menu lateral">
+
+        <ul class="menu grupo archivos publicos">
+          <li class="item acerca">
+            <a class="enlace icon-fa" href="#"><Link to='/about'>Acerca de</Link></a>
+          </li>
+          <li class="item botones">
+            <a class="enlace icon-fa " href="#"><Link to='/botons'>Botones</Link></a>
+          </li>
+
+          <li class="item tipografias">
+            <a class="enlace icon-fa" href="#"><Link to='/fonts'>Tipografías</Link></a>
+          </li>
+          <li class="item tarjetas">
+            <a class="enlace icon-fa focus" href="#"><Link to='/cards'>Tarjetas</Link></a>
+          </li>
+
+
+        </ul>
+
+      </nav>
+
+      <Switch>
+        <Route path="/about" component={About} />
+        <Route path="/botons" component={Botons} />
+        <Route path="/fonts" component={Fonts} />
+        <Route path="/cards" component={Cards} />
+     
+      </Switch>
+    </div>
+
+
   </div>
 
 );
